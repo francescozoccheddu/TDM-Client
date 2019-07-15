62ad8fcfbb3dcbb1ecff31e478ded8e792de79c9
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2019-07-15 18:24:09 +0200

    Add sheet pager

---
+++ app/src/main/java/com/francescozoccheddu/tdmclient/ui/SheetPage2.kt
@@ -0,0 +1,18 @@
+package com.francescozoccheddu.tdmclient.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.francescozoccheddu.tdmclient.R
+
+class SheetPage2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.sheet_page_2, container, false)
+
+}
