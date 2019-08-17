97b868f1cbd8466433a5953cbd00d5fd277654e5
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2019-08-17 11:55:27 +0200

    Improve style

--- app/src/main/java/com/francescozoccheddu/tdmclient/ui/bottomgroup/DurationComponent.kt
+++ app/src/main/java/com/francescozoccheddu/tdmclient/ui/bottomgroup/DurationComponent.kt
@@ -12,6 +12,7 @@ import com.francescozoccheddu.tdmclient.utils.data.snapUp
 import kotlinx.android.synthetic.main.bg_duration.view.bg_duration_cancel
 import kotlinx.android.synthetic.main.bg_duration.view.bg_duration_confirm
 import kotlinx.android.synthetic.main.bg_duration.view.bg_duration_time
+import kotlin.math.roundToInt
 
 class DurationComponent @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -26,15 +27,39 @@ class DurationComponent @JvmOverloads constructor(
 
     init {
         View.inflate(context, R.layout.bg_duration, this)
-        bg_duration_time.thickText = object : KnobView.ThickTextProvider {
-            override fun provide(view: KnobView, track: Int, thick: Int, value: Float): String {
-                val tm = value.toInt()
+
+        val provider = object {
+
+            private val builder = StringBuilder(6)
+
+            operator fun invoke(value: Float): String {
+                val tm = value.roundToInt()
                 val m = tm % 60
-                val h = m / 60
-                return if (h > 0) "${h}h ${m}m" else "${m}m"
+                val h = tm / 60
+                val sb = builder
+                sb.setLength(0)
+                if (h > 0) {
+                    sb.append(h)
+                    sb.append('h')
+                }
+                if (m > 0) {
+                    if (sb.isNotEmpty())
+                        sb.append(' ')
+                    sb.append(m)
+                    sb.append('m')
+                }
+                return sb.toString()
             }
 
         }
+
+        bg_duration_time.thickText = object : KnobView.ThickTextProvider {
+            override fun provide(view: KnobView, track: Int, thick: Int, value: Float) = provider(value)
+        }
+        bg_duration_time.labelText = object : KnobView.LabelTextProvider {
+            override fun provide(view: KnobView, value: Float) = provider(value)
+        }
+
     }
 
     var minTime = 0f
