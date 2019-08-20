95128a6a2a9ab0780a6e80b726b99f4fb5caa55b
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2019-08-20 18:16:34 +0200

    Remove gain label from motion

--- app/src/main/java/com/francescozoccheddu/tdmclient/ui/components/InOutAVD.kt
+++ app/src/main/java/com/francescozoccheddu/tdmclient/ui/components/InOutAVD.kt
@@ -4,7 +4,7 @@ import android.widget.ImageView
 import com.francescozoccheddu.tdmclient.R
 import com.francescozoccheddu.tdmclient.ui.utils.AVD
 
-class CrossAVD(view: ImageView) : AVD<CrossAVD.State>(view, State.GONE) {
+class InOutAVD(view: ImageView) : AVD<InOutAVD.State>(view, State.GONE) {
 
     enum class State(override val state: Int) : AVD.State {
         VISIBLE(R.attr.state_visible), GONE(R.attr.state_gone)
--- app/src/main/java/com/francescozoccheddu/tdmclient/ui/components/UserStatsComponent.kt
+++ app/src/main/java/com/francescozoccheddu/tdmclient/ui/components/UserStatsComponent.kt
@@ -1,51 +1,56 @@
 package com.francescozoccheddu.tdmclient.ui.components
 
 import android.view.ViewGroup
-import android.widget.ImageButton
 import androidx.constraintlayout.motion.widget.MotionLayout
-import androidx.constraintlayout.motion.widget.TransitionAdapter
 import com.francescozoccheddu.tdmclient.R
 
 class UserStatsComponent(parent: ViewGroup) {
 
-    private companion object {
-        private const val CLOSE_BUTTON_ENABLE_PROGRESS = 0.9f
-    }
-
     private val root = parent.findViewById<MotionLayout>(R.id.us_root)
 
     init {
-        val closeButton = parent.findViewById<ImageButton>(R.id.uss_close).apply {
-            setOnClickListener { root.transitionToState(R.id.us_cs_idle) }
+        val closeButton = parent.findViewById<InOutImageButton>(R.id.uss_close).apply {
+            setOnClickListener { requestClose() }
         }
-        val closeButtonAvd = CrossAVD(closeButton)
 
-        root.setTransitionListener(object : TransitionAdapter() {
+        root.setTransition(R.id.us_cs_idle, R.id.us_cs_idle)
+        root.transitionToEnd()
 
-            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
-                closeButtonAvd.state = if (endId == R.id.us_cs_expanded) {
-                    if (startId == R.id.us_cs_expanded || progress >= CLOSE_BUTTON_ENABLE_PROGRESS)
-                        CrossAVD.State.VISIBLE
-                    else
-                        CrossAVD.State.GONE
-                } else if (startId == R.id.us_cs_expanded) {
-                    if (endId == R.id.us_cs_expanded || progress <= 1f - CLOSE_BUTTON_ENABLE_PROGRESS)
-                        CrossAVD.State.VISIBLE
-                    else
-                        CrossAVD.State.GONE
-                } else CrossAVD.State.GONE
-            }
+    }
 
-            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
-                closeButtonAvd.state =
-                    if (currentId == R.id.us_cs_expanded) CrossAVD.State.VISIBLE
-                    else CrossAVD.State.GONE
-            }
+    private fun updateCloseTransition() {
+        root.getTransition(R.id.us_t_idle_to_expanded_sheet).setEnable(draggable && enabled)
+        root.getTransition(R.id.us_t_gone_to_expanded_sheet).setEnable(draggable && !enabled)
+    }
 
-        })
+    private var draggable = true
+        set(value) {
+            root.getTransition(R.id.us_t_idle_to_expanded_gain).setEnable(value)
+            root.getTransition(R.id.us_t_idle_to_expanded_score).setEnable(value)
+            root.getTransition(R.id.us_t_idle_to_expanded_level).setEnable(value)
+            updateCloseTransition()
+        }
+
+    private fun requestClose() {
+        if (root.currentState == R.id.us_cs_expanded)
+            root.transitionToState(
+                if (enabled) R.id.us_cs_idle
+                else R.id.us_cs_gone
+            )
     }
 
     var enabled = true
+        set(value) {
+            if (value != field) {
+                field = value
+                draggable = value
+                root.transitionToState(
+                    if (value) R.id.us_cs_idle
+                    else R.id.us_cs_gone
+                )
+            }
+        }
+
     var score = 0
     var level = 0
     var multiplier = 1f
